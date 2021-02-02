b21ab015cc83181c5625327ffcda14f932d51afe
L. Jiang <l.jiang.1024@gmail.com>
2021-02-02 20:00:24 +0800

    状态列表

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     IonContent,
     IonCard,
     IonText,
     IonCardContent,
-    IonCardHeader,
-    IonCardTitle,
     IonHeader,
     IonPage,
     IonTitle,
@@ -14,6 +12,19 @@ import {
 import './Status.css';
 
 const Status: React.FC = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://monitor.ismdeep.com/api/statuses?token=bbacfa66-b646-4b7c-8192-be150203eb99")
+            .then(res => res.json())
+            .then(result => result["status_list"])
+            .then((items) => {
+                console.log(items);
+                setItems(items);
+            });
+    }, [])
+
+
     return (
         <IonPage>
             <IonHeader>
@@ -22,27 +33,21 @@ const Status: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
-
-                <IonCard>
-                    <IonCardContent>
-                        <p>blog-subscriber.ismdeep.com</p>
-                        <p>Status: <IonText color="success">Alive (25 mins ago)</IonText></p>
-                    </IonCardContent>
-                </IonCard>
-
-                <IonCard>
-                    <IonCardContent>
-                        <p>blog-subscriber.cybart.ist</p>
-                        <p>Status: <IonText color="success">Alive (25 mins ago)</IonText></p>
-                    </IonCardContent>
-                </IonCard>
-
-                <IonCard>
-                    <IonCardContent>
-                        <p>blog-subscriber.www.yinwang.org</p>
-                        <p>Status: <IonText color="success">Alive (25 mins ago)</IonText></p>
-                    </IonCardContent>
-                </IonCard>
+                {
+                    items.map(item => (
+                        <IonCard key={item["key"]}>
+                            <IonCardContent>
+                                <p>{item["key"]}</p>
+                                {item["is_alive"] &&
+                                <p>Status: <IonText color="success">Alive ({item["ago_text"]})</IonText></p>
+                                }
+                                {!item["is_alive"] &&
+                                <p>Status: <IonText color="danger">Down ({item["ago_text"]})</IonText></p>
+                                }
+                            </IonCardContent>
+                        </IonCard>
+                    ))
+                }
 
             </IonContent>
         </IonPage>
