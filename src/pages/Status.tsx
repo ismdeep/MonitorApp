f8183f8967c80ce56a7c52e2c0c597d08bb20823
L. Jiang <l.jiang.1024@gmail.com>
2021-02-08 21:14:19 +0800

    add Loading Toast on Status.tsx

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -7,23 +7,41 @@ import {
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonLoading,
+    IonRefresher,
+    IonRefresherContent
 } from '@ionic/react';
 import './Status.css';
+import { RefresherEventDetail } from '@ionic/core';
+
 
 const Status: React.FC = () => {
     const [items, setItems] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
-    useEffect(() => {
+    function doRefresh(event: CustomEvent<RefresherEventDetail>) {
+        console.log("Hello");
+        event.detail.complete();
+        setItems([]);
+        setLoaded(false);
+    }
+
+
+    // useEffect(() => {
+    //
+    // }, [])
+
+    if (items.length <= 0) {
         fetch("https://monitor.ismdeep.com/api/statuses?token=bbacfa66-b646-4b7c-8192-be150203eb99")
             .then(res => res.json())
             .then(result => result["status_list"])
             .then((items) => {
                 console.log(items);
                 setItems(items);
+                setLoaded(true);
             });
-    }, [])
-
+    }
 
     return (
         <IonPage>
@@ -33,20 +51,32 @@ const Status: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
-                {
-                    items.map(item => (
-                        <IonCard key={item["key"]}>
-                            <IonCardContent>
-                                <p>{item["key"]}</p>
-                                {item["is_alive"] &&
-                                <p>Status: <IonText color="success">Alive ({item["ago_text"]})</IonText></p>
-                                }
-                                {!item["is_alive"] &&
-                                <p>Status: <IonText color="danger">Down ({item["ago_text"]})</IonText></p>
-                                }
-                            </IonCardContent>
-                        </IonCard>
-                    ))
+                <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+                    <IonRefresherContent></IonRefresherContent>
+                </IonRefresher>
+
+                {!loaded &&
+                <IonCard>
+                    <IonCardContent>
+                        <IonText>Loading...</IonText>
+                    </IonCardContent>
+                </IonCard>
+                }
+
+                {loaded &&
+                items.map(item => (
+                    <IonCard key={item["key"]}>
+                        <IonCardContent>
+                            <p>{item["key"]}</p>
+                            {item["is_alive"] &&
+                            <p>Status: <IonText color="success">Alive ({item["ago_text"]})</IonText></p>
+                            }
+                            {!item["is_alive"] &&
+                            <p>Status: <IonText color="danger">Down ({item["ago_text"]})</IonText></p>
+                            }
+                        </IonCardContent>
+                    </IonCard>
+                ))
                 }
 
             </IonContent>
